4c950667918143233dda684ddaae3a20bc548216
Maverlous Ikponmwosa <maverlousikponmwosa@Macbook-Pro.local>
2021-10-30 18:33:33 +0100

    added challenge file

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 use App\Helper\ProcessFile\ProcessFactory;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller{
     public function upload(Request $request){
-        $file = storage_path('app/files')."/challenge__1_.json";
+
+        $file = public_path()."/files/challenge__1_.json";
         $process = new ProcessFactory();  //initialize process factory
         $process = $process->initialize('json'); //pass file type. default is json
         $process->process($file);
