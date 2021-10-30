3001923f908df4204762374a31f765cba060a916
Maverlous Ikponmwosa <maverlousikponmwosa@Macbook-Pro.local>
2021-10-30 12:42:15 +0100

    implemented chunking data and batch dispatching and  created job file

--- app/Helper/ProcessFile/ProcessJSON.php
+++ app/Helper/ProcessFile/ProcessJSON.php
@@ -1,10 +1,19 @@
 <?php
 namespace App\Helper\ProcessFile;
 
+use App\Libraries\DateLib;
+use Illuminate\Support\Facades\Bus;
+
 class ProcessJSON implements ProcessInterface {
 
     public function process($file)
     {
         $file_data = json_decode(file_get_contents($file), true);
+        $date_lib = new DateLib();
+        $chunks = array_chunk($file_data, 1000);
+        $batch = Bus::batch([])->dispatch();
+        foreach ($chunks as $key => $chunk){
+            $batch->add();
+        }
     }
 }
---
+++ app/Jobs/UploadFile.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Libraries\DateLib;
+use App\Models\CreditCard;
+use App\Models\User;
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UploadFile implements ShouldQueue
+{
+    use Batchable, Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    public $data;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $date_lib = new DateLib();
+        foreach ($this->data as $key =>  $value){
+            $credit_card = $value['credit_card'];
+            unset($value['credit_card']);
+            $date = $date_lib->covertToTime($value['date_of_birth']);
+            $age = $date_lib->getAge($date);
+            if($age === 18 || $age === 65 || !$age ){
+                $save_user = User::Create($value);
+                $credit_cards = new CreditCard($credit_card);
+                $save_user->CreditCard()->save($credit_cards);
+            }
+
+        }
+    }
+}
