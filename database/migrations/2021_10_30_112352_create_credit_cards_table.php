b9ff6281e30f3caca25caddb61e8fc6903cedd71
Maverlous Ikponmwosa <maverlousikponmwosa@Macbook-Pro.local>
2021-10-30 12:25:12 +0100

    created models and migrations

--- database/migrations/2021_10_30_112352_create_credit_cards_table.php
+++ database/migrations/2021_10_30_112352_create_credit_cards_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateCreditCardsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('credit_cards', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->integer('user_id');
+            $table->string('type');
+            $table->string('number');
+            $table->text('name');
+            $table->string('expirationDate');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('credit_cards');
     }
 }
